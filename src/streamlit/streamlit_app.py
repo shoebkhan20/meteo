e5a03d2553d36aea702661c1eb573e6c0fe2672e
shoebkhan20 <shoeb.khan20@gmail.com>
2024-10-21 22:02:52 +0200

    Continue

--- src/streamlit/streamlit_app.py
+++ src/streamlit/streamlit_app.py
@@ -13,44 +13,56 @@ df = dfDate.drop('Date',axis=1)
 
 st.title("Prédictions météo - by SKH")
 
-st.write("Exercices de prédiction")
+pages=['Exercices de prédiction aléatoire','Météo de demain ?']
+page=st.sidebar.radio("Aller vers",pages)
 
-modelRadio=st.radio('Sélectionner votre model',['RandomForestDecisionTreeClassifier','KNeighborsClassifier','SVC','XGBClassifier'])
-listeModels=['RandomForestDecisionTreeClassifier','KNeighborsClassifier','SVC','XGBClassifier']
+if page == pages[0]:
 
-def displayWeatherImage(meteo):
-    if (meteo == 0):
-        st.image("data/images/sun.png",width=30)
-    else:
-        st.image("data/images/rain.png",width=30)
+    st.write("Exercices de prédiction aléatoire")
 
-def predict(modelName):
-    modelPath="models/"+modelName
-    model=joblib.load(modelPath)
-    aleatoire=random.randint(0,df.shape[0])
-    pred=model.predict([df.loc[aleatoire]])
-    col1, col2 = st.columns(2)
-    with col1:
-         st.write("Météo le",dfDate.loc[aleatoire]["Date"]," à",dfLocation.loc[aleatoire],":")
-         st.write("Prédiction pour le lendemain :")
-         st.write("Réalité le lendemain :")
-    with col2:
-         displayWeatherImage(round(df['RainToday'].loc[df.index == aleatoire].values[0]))
-         displayWeatherImage(round(pred[0]))
-         displayWeatherImage(round(dfRaintomorrow.loc[aleatoire]))
+    listeModels=['clf_dt','clf_knn','clf_svm','clf_xgb']
+    listeModelTitles=['RandomForestDecisionTreeClassifier','KNeighborsClassifier','SVC','XGBClassifier']
 
-    
+    def displayWeatherImage(meteo):
+        if (meteo == 0):
+            st.image("data/images/sun.png",width=30)
+        else:
+            st.image("data/images/rain.png",width=30)
 
-if st.button("Lancer un test de prédiction de météo aléatoire"):
-        if modelRadio=="RandomForestDecisionTreeClassifier":
-              predict("clf_dt")
-        if modelRadio=="KNeighborsClassifier":
-              predict("clf_knn")
-        if modelRadio=="SVC":
-              predict("clf_svm")
-        if modelRadio=="XGBClassifier":
-              predict("clf_xgb")
-        
+    def predict(listeModels,aleatoire):
+        list_to_return=[]
+        for modelName in listeModels:
+            modelPath="models/"+modelName
+            model=joblib.load(modelPath)
+            pred=model.predict([df.loc[aleatoire]])
+            list_to_return.append(pred)
+        return list_to_return
+
+
+
+    if st.button("Lancer un test de prédiction de météo aléatoire"):
+        aleatoire=random.randint(0,df.shape[0])
+        col1, col2 = st.columns(2)
+        listPredictions=predict(listeModels,aleatoire)
+        with col1:
+            st.write("##### Météo le",dfDate.loc[aleatoire]["Date"]," à",dfLocation.loc[aleatoire]," #####")
+            for title in listeModelTitles:
+                st.write("Prédiction pour ",title)
+            st.write("##### Réalité le lendemain #####")
+        with col2:
+            displayWeatherImage(round(df['RainToday'].loc[df.index == aleatoire].values[0]))
+            for p in listPredictions:
+                displayWeatherImage(round(p[0]))
+            displayWeatherImage(round(dfRaintomorrow.loc[aleatoire]))
+
+if page == pages[1]:
+
+    st.write("Prédiction de la météo demain en Australie")
+
+    option = st.selectbox(
+    "Choisissez une localité en Australie",
+    ('Albury','BadgerysCreek','Cobar','CoffsHarbour','Moree','Newcastle','NorahHead','NorfolkIsland','Penrith','Richmond','Sydney','SydneyAirport','WaggaWagga','Williamtown','Wollongong','Canberra','Tuggeranong','MountGinini','Ballarat','Bendigo','Sale','MelbourneAirport','Melbourne','Mildura','Nhil','Portland','Watsonia','Dartmoor','Brisbane','Cairns','GoldCoast','Townsville','Adelaide','MountGambier','Nuriootpa','Woomera','Albany','Witchcliffe','PearceRAAF','PerthAirport','Perth','SalmonGums','Walpole','Hobart','Launceston','AliceSprings','Darwin','Katherine','Uluru'),)
+
 
 
 
