1a0eb702d3095b81123f5efffeb6f04585b8ef1b
shoebkhan20 <shoeb.khan20@gmail.com>
2024-10-21 12:29:09 +0200

    test

---
+++ src/models/predict_model.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import joblib
+import random
+
+print("importing processed dataset\n")
+
+dfDate=pd.read_csv('data/processed/weatherAUS_processed_data_with_date.csv')
+dfDate = dfDate.drop('Unnamed: 0',axis=1)
+dfRaintomorrow=dfDate['RainTomorrow']
+dfDate=dfDate.drop('RainTomorrow',axis=1)
+df = dfDate.drop('Date',axis=1)
+
+#clf_rf=joblib.load("models/clf_rf")
+clf_dt=joblib.load("models/clf_dt")
+
+#random.seed(1)
+
+for i in range(10):
+    aleatoire=random.randint(0,df.shape[0])
+
+    pred=clf_dt.predict([df.loc[aleatoire]])
+
+    print("ligne :",aleatoire)
+    print("Pluie le",dfDate.loc[aleatoire]["Date"],":",round(df['RainToday'][0]))
+    print("Prédiction pour le lendemain :",round(pred[0]))
+    print("Réalité le lendemain :",round(dfRaintomorrow.loc[aleatoire]),"\n")
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -16,13 +16,55 @@ from sklearn.metrics import accuracy_score
 import xgboost as xgb
 import joblib
 
+print("importing processed datasets\n")
 X_smo=pd.read_csv('data/processed/X_smo.csv')
 y_smo=pd.read_csv('data/processed/y_smo.csv')
-
 X_smo = X_smo.drop('Unnamed: 0',axis=1)
 y_smo = y_smo.drop('Unnamed: 0',axis=1)
 
+X_rus=pd.read_csv('data/processed/X_rus.csv')
+y_rus=pd.read_csv('data/processed/y_rus.csv')
+X_rus = X_rus.drop('Unnamed: 0',axis=1)
+y_rus = y_rus.drop('Unnamed: 0',axis=1)
+
+X_cc=pd.read_csv('data/processed/X_cc.csv')
+y_cc=pd.read_csv('data/processed/y_cc.csv')
+X_cc = X_cc.drop('Unnamed: 0',axis=1)
+y_cc = y_cc.drop('Unnamed: 0',axis=1)
+
+X_ros=pd.read_csv('data/processed/X_ros.csv')
+y_ros=pd.read_csv('data/processed/y_ros.csv')
+X_ros = X_ros.drop('Unnamed: 0',axis=1)
+y_ros = y_ros.drop('Unnamed: 0',axis=1)
+
+print("Fitting RandomForestClassifier model\n")
+clf_rf = RandomForestClassifier(n_jobs = -1, random_state = 42)
+clf_rf.fit(X_ros, y_ros)
+
+print("Fitting RandomForestDecisionTreeClassifierClassifier model\n")
+clf_dt = DecisionTreeClassifier(criterion ='entropy', max_depth=4, random_state = 42)
+clf_dt.fit(X_rus, y_rus)
+
+print("Fitting KNeighborsClassifier model\n")
+clf_knn = KNeighborsClassifier(n_neighbors=7, metric='minkowski')
+clf_knn.fit(X_cc, y_cc)
+
+print("Fitting SVC model\n")
+clf_svm = svm.SVC(gamma=0.1, kernel='poly', random_state=42, probability=True) 
+clf_svm.fit(X_cc, y_cc)
+
+print("Fitting LogisticRegression model\n")
+clf_lr = LogisticRegression(C=1.0,random_state=42)
+clf_lr.fit(X_ros, y_ros)
+
+print("Fitting XGBClassifier model\n")
 clf_xgb = xgb.XGBClassifier(random_state=42, eval_metric='logloss')
 clf_xgb.fit(X_smo, y_smo)
 
+print("Saving models\n")
+joblib.dump(clf_rf,"models/clf_rf")
+joblib.dump(clf_dt,"models/clf_dt")
+joblib.dump(clf_knn,"models/clf_knn")
+joblib.dump(clf_svm,"models/clf_svm")
+joblib.dump(clf_lr,"models/clf_lr")
 joblib.dump(clf_xgb,"models/clf_xgb")
