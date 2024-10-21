af618798b0f7cc8e02cc068b2b6c53101c55d61b
shoebkhan20 <shoeb.khan20@gmail.com>
2024-10-21 12:30:59 +0200

    comment

--- src/streamlit/streamlit_app.py
+++ src/streamlit/streamlit_app.py
@@ -13,16 +13,16 @@ st.title("Prédictions météo - by SKH")
 
 st.write("Exercices de prédiction")
 
-modelRadio=st.radio('Sélectionner votre model',['RandomForestClassifier','RandomForestDecisionTreeClassifier','KNeighborsClassifier','SVC','XGBClassifier'])
+#modelRadio=st.radio('Sélectionner votre model',['RandomForestClassifier','RandomForestDecisionTreeClassifier','KNeighborsClassifier','SVC','XGBClassifier'])
 
-if st.button("Lancer un test de prédiction de météo"):
-    if modelRadio=="RandomForestDecisionTreeClassifier":
-        clf_dt=joblib.load("models/clf_dt")
-        aleatoire=random.randint(0,df.shape[0])
-        pred=clf_dt.predict([df.loc[aleatoire]])
-        st.write("Pluie le",dfDate.loc[aleatoire]["Date"],":",round(df['RainToday'][0]))
-        st.write("Prédiction pour le lendemain :",round(pred[0]))
-        st.write("Réalité le lendemain :",round(dfRaintomorrow.loc[aleatoire]),"\n")
+#if st.button("Lancer un test de prédiction de météo"):
+#    if modelRadio=="RandomForestDecisionTreeClassifier":
+#        clf_dt=joblib.load("models/clf_dt")
+#        aleatoire=random.randint(0,df.shape[0])
+#        pred=clf_dt.predict([df.loc[aleatoire]])
+#        st.write("Pluie le",dfDate.loc[aleatoire]["Date"],":",round(df['RainToday'][0]))
+#        st.write("Prédiction pour le lendemain :",round(pred[0]))
+#        st.write("Réalité le lendemain :",round(dfRaintomorrow.loc[aleatoire]),"\n")
 
 
 
